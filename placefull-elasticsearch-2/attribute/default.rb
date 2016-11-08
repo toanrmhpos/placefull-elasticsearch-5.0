a0f9ebfbb85eccfd414b253add3f74996db3d454
Shane Menchions <shanemenchions@gmail.com>
2016-11-08 12:11:14 -0800

    try java again

--- placefull-elasticsearch-2/attribute/default.rb
+++ placefull-elasticsearch-2/attribute/default.rb
@@ -6,4 +6,4 @@
 
 default["java"]["install_flavor"] = "oracle"
 default["java"]["jdk_version"] = "8"
-default["java"]["oracle"]["accept_oracle_download_terms"] = "true"
+default["java"]["oracle"]["accept_oracle_download_terms"] = true
