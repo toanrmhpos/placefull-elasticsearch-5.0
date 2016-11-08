14289a56fa3979e5bdc0c24e095e1a2303b986d5
Shane Menchions <shanemenchions@gmail.com>
2016-11-08 12:26:05 -0800

    and again

--- placefull-elasticsearch-2/attribute/default.rb
+++ placefull-elasticsearch-2/attribute/default.rb
@@ -4,6 +4,9 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
+override["java"]["oracle"]["accept_oracle_download_terms"] = true
+override["java"]["accept_license_agreement"] = true
+
 default["java"]["install_flavor"] = "oracle"
 default["java"]["jdk_version"] = "8"
-default["java"]["oracle"]["accept_oracle_download_terms"] = true
+
